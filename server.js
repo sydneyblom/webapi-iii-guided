ce4fd63e1d86dd505a7c7f6356022a0de17183d1
Sydney Blom <sydneyblom@#gmail.com>
2019-10-30 12:33:13 -0500

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet =require('helmet')
+const morgan =require('morgan')
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
@@ -9,13 +10,33 @@ function dateLogger(req, res, next) {
 }
 
 function callHttp (req, res, next){
-  console.log(req.originalUrl)
+  console.log(`${req.method} to ${req.url}`)
   next();
 }
+
+function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+  const password = req.headers.password || '';
+  if (password == '')  {
+    res.status(400).json({ you: 'please provide a password' });
+  }
+  else if (password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(400).json({ you: 'cannot pass!!' });
+  }
+
+}
+
+
 //globalmiddleware
 server.use(helmet());
 server.use(express.json());
+server.use(gateKeeper);
 server.use(dateLogger);
+server.use(callHttp);
+server.use(morgan('dev'))
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
